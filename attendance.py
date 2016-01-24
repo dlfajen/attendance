3314cd9666eb45553614a0a9a26beffd8aaed5a5
Deana Fajen <dlfajen@hotmail.com>
2016-01-23 21:26:47 -0600

    Added known guild members, hours accumulated, and largest period of inactivity
    Added known guild members, hours accumulated, and largest period of
    inactivity

--- attendance.py
+++ attendance.py
@@ -1,38 +1,29 @@
 from datetime import datetime
+from datetime import timedelta
 import re
 
-CHAR_VERIFIED_REG_EX_LIST = ["^(\S+)\sbegins to cast a spell.$",
-                             "^(\S+)\s(backstabs|bashes|bites|crushes|hits|kicks|pierces|punches|slashes)\s.+\sfor\s[0-9]+\spoint[s]* of damage.$",
-                             "^(\S+)\ssays,\s.+$",
-                             "^(\S+)'s\shand is covered with a dull aura.$",
-                             "^(\S+)\sbegins to walk faster.$",
-                             "^(\S+)\sis completely healed.$",
-                             "^(\S+)\slooks\s.+.$",
-                             "^(\S+)\sbegins to regenerate.$",
-                             "^(\S+)'s\seyes gleam with heroic resolution.$",
-                             "^(\S+)\sfeels much better.",
-                             "^(\S+)\shas been struck by lightning.",
-                             "^(\S+)\screates a shimmering dimensional portal.",
-                             "^(\S+)'s\seye gleams with the power of Aegolism.",
-                             "^(\S+)\sfeels the favor of the gods upon them.",
-                             "^(\S+)\ssteps into the shadows and disappears.",
-                             "^(\S+)\sfades away.",
-                             "^(\S+)\sis surrounded by a summer haze.",
-                             "^(\S+)'s\simage shimmers.",
-                             "^(\S+)'s\swounds fade away.",
-                             "^(\S+)'s\sskin sears.",
-                             "^(\S+)'s\ssong ends abruptly."
+CHAR_VERIFIED_REG_EX_LIST = ["^([a-z|A-Z]{3,})\s(backstabs|bashes|bites|crushes|hits|kicks|pierces|punches|slashes)\s.+\sfor\s[0-9]+\spoint[s]* of damage.$",
+                             "^([a-z|A-Z]{3,})\ssays,\s.+$",
+                             "^([a-z|A-Z]{3,})'s\s(armor|blood|body|brain|ears|eyes|face|feet|fist|flesh|hair|hammer|hand|hands|head|image|mind|muscles|skin|song|veins|weapon|weapons|world|wounds)\s.+.$",
+                             "^([a-z|A-Z]{3,})\s(becomes|begins|blinks|blisters|breathes|calls|calms|casts|chokes|clutches|convulses|creates|disappears|dissolves|doesn't|dons|doubles|exhales|fades|feels|flees|floats|gains|gasps|glances|goes|grins|grows|has|inhales|is|lets|lights|looks|opens|peers|pulses|rises|screams|sends|shrieks|shivers|shudders|sighs|simmers|singes|sings|slows|smiles|spews|spouts|staggers|stands|starts|steps|stops|strikes|summons|sweats|takes|turns|winces|writhes)\s.+.$",
+                             "^([a-z|A-Z]{3,})\s(blinks|combusts|dies|fades|moans|pales|panics|rages|shrinks|staggers|stumbles|weakens|winces|yawns).$",
+                             "^([a-z|A-Z]{3,})\sgoes into a berserker frenzy!"
                             ]
                             
-MOB_VERIFIED_REG_EX_LIST = ["^.+\s(backstabs|bashes|bites|crushes|hits|kicks|pierces|punches|slashes)\s(\S+)\sfor\s[0-9]+\spoint[s]* of damage.$"
+MOB_VERIFIED_REG_EX_LIST = ["^.+\s(backstabs|bashes|bites|crushes|hits|kicks|pierces|punches|slashes)\s([a-z|A-Z]{3,})\sfor\s[0-9]+\spoint[s]* of damage.$"
                   ]              
 
 PET_VERIFIED_REG_EX_LIST = ["^(\S+)\ssays 'Sorry, Master..calming down.'$",
-                            "^(\S+)\ssays 'At your service Master.'$"]
+                            "^(\S+)\ssays 'At your service Master.'$",
+                            "^(\S+)\ssays 'Guarding with my life..oh splendid one.'$",
+                            "^(\S+)\ssays 'No longer taunting attackers, Master.'$"]
+                            
+GUILD_VERIFIED_REG_EX_LIST = ["([a-z|A-Z]{3,})\stells the guild,\s.+$"]                            
                   
 verified_hits = dict()
 dkp_lines = list()
 pet_list = list()
+guild_list = list()
 
 file_name = raw_input("Enter log file name: ")
 
@@ -72,21 +63,33 @@ for line in file:
                     m = re.search(regex, log_data)
                     if m:
                         if m.group(1) in verified_hits:
+                            # Check elapsed time since last update
+                            elapsed = date_object - verified_hits [m.group(1)][2] 
+                            # If elapsed time greater than current elapsed time, update it
+                            if verified_hits[m.group(1)][3] < elapsed.seconds:
+                                verified_hits[m.group(1)][3] = elapsed.seconds
+                                
                             # Update last entry
-                            verified_hits[m.group(1)][1] = date_object
+                            verified_hits[m.group(1)][2] = date_object
                         else:
                             # If name not in list, add timestamp to both first and last entries
-                            verified_hits[m.group(1)] = [date_object, date_object]
+                            verified_hits[m.group(1)] = ['Unknown', date_object, date_object, 0]
             
                 for regex in MOB_VERIFIED_REG_EX_LIST:
                     m = re.search(regex, log_data)
                     if m:
                         if m.group(2) in verified_hits:
+                            # Check elapsed time since last update
+                            elapsed = date_object - verified_hits [m.group(2)][2]
+                            # If elapsed time greater than current elapsed time, update it
+                            if verified_hits[m.group(2)][3] < elapsed.seconds:
+                                verified_hits[m.group(2)][3] = elapsed.seconds
+                                
                             # Update last entry
-                            verified_hits[m.group(2)][1] = date_object
+                            verified_hits[m.group(2)][2] = date_object
                         else:
                             # If name not in list, add timestamp to both first and last entries
-                            verified_hits[m.group(2)] = [date_object, date_object]
+                            verified_hits[m.group(2)] = ['Unknown', date_object, date_object, 0]
                  
                 for regex in PET_VERIFIED_REG_EX_LIST:
                     m = re.search(regex, log_data)
@@ -94,6 +97,13 @@ for line in file:
                         # If pet name not in list, add it
                         if m.group(1) not in pet_list:
                             pet_list.append(m.group(1))
+                            
+                for regex in GUILD_VERIFIED_REG_EX_LIST:
+                    m = re.search(regex, log_data)
+                    if m:
+                        # If guild member name not in list, add it
+                        if m.group(1) not in guild_list:
+                            guild_list.append(m.group(1))
                 
         elif date_object > raid_end_time:
             # Past raid end time, break out of loop
@@ -103,10 +113,14 @@ for line in file:
 for pet in pet_list:
     if pet in verified_hits:
         del verified_hits[pet]
+        
+for guild_member in guild_list:
+    if guild_member in verified_hits:
+        verified_hits[guild_member][0] = "Guild Member"
             
 out_file = open("attendance.txt", "w")            
 for k, v in sorted(verified_hits.items()):
-    out_file.write(k + ", " + str(v[0]) + ", " + str(v[1]) + "\n")
+    out_file.write(k + ", " + str(v[0]) + ", " + str(v[1]) + ", " + str(v[2]) + ", " + str(v[2] - v[1]) + ", " + str(v[3]/60) + "\n")
 
 for line in dkp_lines:
     out_file.write(line)
